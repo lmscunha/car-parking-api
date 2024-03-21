eb878b96e12b7ad3e9dcdb4deaa6aa5fb73faea3
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-03-21 07:10:44 -0300

    feat: add register route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\Auth;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('auth/register', Auth\RegisterController::class);
