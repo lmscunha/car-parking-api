3b9490470c46bdaf3feaf80fb197e34e500b8bd8
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-03-23 11:30:08 -0300

    feat: add logout to routes

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [Auth\ProfileController::class, 'show']);
     Route::put('profile', [Auth\ProfileController::class, 'update']);
     Route::put('password', Auth\PasswordUpdateController::class);
+    Route::post('auth/logout', Auth\LogoutController::class);
 });
