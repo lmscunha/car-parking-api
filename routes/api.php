2bc154ace8634e1cabf8d4222b876d6268a06dda
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-03-25 08:08:19 -0300

    feat: add zone controller to routes api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Auth;
 use App\Http\Controllers\Api\V1\VehicleController;
+use App\Http\Controllers\Api\V1\ZoneController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,6 +12,9 @@ Route::get('/user', function (Request $request) {
 
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
+
+Route::get('zones', [ZoneController::class, 'index']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [Auth\ProfileController::class, 'show']);
     Route::put('profile', [Auth\ProfileController::class, 'update']);
