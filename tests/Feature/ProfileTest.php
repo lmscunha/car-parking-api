7891f597a08ef4c80571abd662bdcabc629e0355
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-03-31 18:44:56 -0300

    test: add UserCanGetTheirProfile

---
+++ tests/Feature/ProfileTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProfileTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserCanGetTheirProfile()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->getJson('/api/v1/profile');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['name', 'email'])
+            ->assertJsonCount(2)
+            ->assertJsonFragment(['name' => $user->name]);
+    }
+}
