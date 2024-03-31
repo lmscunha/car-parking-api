aa1e3e2c08302aca8045e513921d4a3a4f062053
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-03-31 18:50:15 -0300

    test: add UserCanUpdateNameAndEmail

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -22,4 +22,24 @@ class ProfileTest extends TestCase
             ->assertJsonCount(2)
             ->assertJsonFragment(['name' => $user->name]);
     }
+
+    public function testUserCanUpdateNameAndEmail()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->putJson('/api/v1/profile', [
+            'name' => 'John Updated',
+            'email' => 'john_updated@example.com',
+        ]);
+
+        $response->assertStatus(202)
+            ->assertJsonStructure(['name', 'email'])
+            ->assertJsonCount(2)
+            ->assertJsonFragment(['name' => 'John Updated']);
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'John Updated',
+            'email' => 'john_updated@example.com'
+        ]);
+    }
 }
