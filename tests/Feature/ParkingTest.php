58485a989f7c0db72fb95480969dcab0d9216d4d
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-04-08 18:52:07 -0300

    test: create UserCanGetOngoingParkingWithCorrectPrice

--- tests/Feature/ParkingTest.php
+++ tests/Feature/ParkingTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Parking;
 use App\Models\User;
 use App\Models\Vehicle;
 use App\Models\Zone;
@@ -36,5 +37,32 @@ class ParkingTest extends TestCase
             ]);
 
         $this->assertDatabaseCount('parkings', '1');
+    }
+
+    public function testUserCanGetOngoingParkingWithCorrectPrice()
+    {
+        $user = User::factory()->create();
+        $vehicle = Vehicle::factory()->create(['user_id' => $user->id]);
+        $zone = Zone::first();
+
+        $this->actingAs($user)->postJson('/api/v1/parkings/start', [
+            'vehicle_id' => $vehicle->id,
+            'zone_id' => $zone->id
+        ]);
+
+        $this->travel(2)->hours();
+
+        $parking = Parking::first();
+        $response = $this->actingAs($user)->getJson('/api/v1/parkings/' . $parking->id);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data'])
+            ->assertJson([
+                'data' => [
+                    'stop_time' => null,
+                    // Todo check set result in 200
+                    'total_price' => ($zone->price_per_hour * 2) + 1,
+                ]
+            ]);
     }
 }
